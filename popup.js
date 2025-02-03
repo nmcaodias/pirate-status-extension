8883fe5130b53b67a7026422027e89bb553f10a7
Nuno Dias <azoresnuno@hotmail.com>
2025-02-03 21:20:34 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const titleList = document.getElementById("titleList");
 
     // Load saved titles
-    chrome.storage.local.get("titles", function (data) {
-        if (data.titles) {
-            data.titles.forEach(title => addTitleToList(title));
+    chrome.storage.local.get("info", function (data) {
+        if (data.info) {
+            data.info.forEach(info => addInfoToList(info.title, info.url, info.showType));
         }
     });
 
@@ -26,38 +26,43 @@ document.addEventListener("DOMContentLoaded", function () {
                     const title = response.title;
                     const url = response.url;
                     const showType = response.showType;
+                    const tempInfo = {title: title, url: url, showType: showType};
                     
+                    chrome.storage.local.get("info", function (data) {
+                        const info = data.info || [];
+                        info.push(tempInfo);
+                        chrome.storage.local.set({ "info": info }, function () {
+                            addInfoToList(title, url, showType);
+                        });
+                    });
+                    /*
                     chrome.storage.local.get("titles", function (data) {
                         const titles = data.titles || [];
                         titles.push(title);
                         chrome.storage.local.set({ "titles": titles }, function () {
-                            addTitleToList(title, url, showType);
+                            addInfoToList(title, url, showType);
                         });
                     });
+                    */
                 }
                 
             });
         });
     });
 
-    function addTitleToList(title, url, showType) {
+    function addInfoToList(title, url, showType) {
         const li = document.createElement("li");
 
         const titleDiv = document.createElement("div");
         titleDiv.innerHTML = "<b>Title: </b>" + title;
-
+        li.appendChild(titleDiv);
+        
         const urlDiv = document.createElement("div");
         urlDiv.innerHTML = "<b>URL: </b><a href=" + url + ">" + url + "</a>";
+        li.appendChild(urlDiv);
 
         const typeDiv = document.createElement("div");
         typeDiv.innerHTML = "<b>Show Type: </b>" + showType;
-
-        // newDiv.append("Title: " + title);
-        // newDiv.append("URL: " + url);
-        // newDiv.append("Show Type: " + showType);
-
-        li.appendChild(titleDiv);
-        li.appendChild(urlDiv);
         li.appendChild(typeDiv);
         
         titleList.appendChild(li);
